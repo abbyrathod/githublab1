ffc198dbde2128f8cb4135c7bc944887ccb3d603
abbyrathod <abhayrathod8@gmail.com>
2018-10-16 09:26:49 +0530

    Create LinkedList.c
    Linkedlist file to insert any element at first position and see the whole list.

---
+++ LinkedList.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct node {
+   int data;
+   int key;
+   struct node *next;
+};
+
+struct node *head = NULL;
+struct node *current = NULL;
+
+void main() {
+   insertFirst(1,10);
+   insertFirst(2,20);
+   insertFirst(3,30);
+   insertFirst(4,1);
+   insertFirst(5,40);
+   insertFirst(6,56); 
+
+   printf("Original List: "); 
+	
+   //print list
+   printList();
+	
+   printf("\nList after deleting all items: ");
+   printList();
+   insertFirst(1,10);
+   insertFirst(2,20);
+   insertFirst(3,30);
+   insertFirst(4,1);
+   insertFirst(5,40);
+   insertFirst(6,56);
+   
+   printf("\nNew List: ");
+   printList();
+   printf("\n");  
+   
+   
+   void printList() {
+   struct node *ptr = head;
+   printf("\n[ ");
+	
+   //start from the beginning
+   while(ptr != NULL) {
+      printf("(%d,%d) ",ptr->key,ptr->data);
+      ptr = ptr->next;
+   }
+	
+   printf(" ]");
+}
+
+//insert link at the first location
+void insertFirst(int key, int data) {
+   //create a link
+   struct node *link = (struct node*) malloc(sizeof(struct node));
+	
+   link->key = key;
+   link->data = data;
+	
+   //point it to old first node
+   link->next = head;
+	
+   //point first to new first node
+   head = link;
+}
